6fd208c509af660f9ee84009ab722d6aa4f42490
jasperan <23caj23@gmail.com>
2023-04-26 04:10:30 +0200

    feat: added docstrings

--- whatsappbeacon.py
+++ whatsappbeacon.py
@@ -27,7 +27,19 @@ from db_to_excel import Converter
 '''
 
 
-def study_user(driver, user, language,excel):
+def study_user(driver, user, language, excel):
+	"""
+	Generates an excel file with user data and checks their online status.
+
+	Parameters:
+	driver (webdriver): Selenium webdriver instance.
+	user (str): The name of the user to study.
+	language (str): The language of the user's WhatsApp account.
+	excel (bool): Whether or not to generate an excel file.
+
+	Returns:
+	None
+	"""
 	# Create Excel File
 	if excel:
 		excel = Converter()
@@ -121,6 +133,15 @@ def study_user(driver, user, language,excel):
 
 
 def whatsapp_login():
+	"""
+	Logs into WhatsApp Web using Selenium and returns the driver object.
+
+	Raises:
+	- InvalidArgumentException: If a Selenium navigator is already running in the background.
+
+	Returns:
+	- driver: A webdriver object with WhatsApp Web open and the user logged in.
+	"""
 	try:
 		print('In order to make this program to work, you will need to log-in once in WhatsApp. After that, your session will be saved until you revoke it.')
 		options = webdriver.ChromeOptions()
@@ -140,6 +161,17 @@ def whatsapp_login():
 
 
 def main():
+	"""
+	This function creates a table in the database and parses command line arguments.
+	It then logs into WhatsApp and studies the specified user in the specified language.
+	If the excel flag is set, it converts the database to an Excel file.
+
+	Args:
+	None
+
+	Returns:
+	None
+	"""
 
 	Database.create_table()
 
